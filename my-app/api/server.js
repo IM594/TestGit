f828e10cde53115df839fcddc094a7822f440480
Zhaohao Lu <zhlu7335@uni.sydney.edu.au>
2023-04-22 05:09:01 +1000

    解决了跨域问题

--- my-app/api/server.js
+++ my-app/api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require("cors")
+
+app.use(cors())
+
 app.use(express.json());
 
 app.get("/", function(req, res) {
-    res.send("Hello World! It's working!");
-    });
+    res.json({"name": "张三"})
+  })
 
 app.listen(3000, () => {
     console.log("app listening on port 3000!");
--- my-app/client/src/App.js
+++ my-app/client/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { useEffect, useState } from "react";
+
+export default function App() {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000")
+      .then((res) => res.json())
+      .then((data) => setName(data.name));
+  }, []);
+
+  return <div>Hello {name}</div>;
 }
-
-export default App;
